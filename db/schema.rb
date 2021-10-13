cd574b2a0395c99b85491de44f353e9b5bf81558
sevader14 <vsevolod.deriushkin@gmail.com>
2021-10-13 10:26:58 +0200

    adds service requests model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_082033) do
+ActiveRecord::Schema.define(version: 2021_10_13_082547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_10_13_082033) do
 
   create_table "service_providers", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "service_requests", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
