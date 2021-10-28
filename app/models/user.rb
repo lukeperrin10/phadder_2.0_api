754b85cb7ae90c69b32b8552f1c7587e050a8fc3
luke <lukeperrin27@gmail.com>
2021-10-28 20:40:07 +0200

    adds roles to user model and sets default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
+  after_initialize :set_default_role, if: :new_record?
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  enum role: %i[end_user service_provider super_admin]
+
+  private
+
+  def set_default_role
+    self.role ||= :end_user
+  end
 end
