429e2d74117099a00e9504b1b6ca36ae7fe7109f
luke <lukeperrin27@gmail.com>
2021-10-28 13:17:53 +0200

    fixes migration version to 6.1

--- db/migrate/20211013082033_create_join_table_categories_service_providers.rb
+++ db/migrate/20211013082033_create_join_table_categories_service_providers.rb
@@ -1,4 +1,4 @@
-class CreateJoinTableCategoriesServiceProviders < ActiveRecord::Migration[7.0]
+class CreateJoinTableCategoriesServiceProviders < ActiveRecord::Migration[6.1]
   def change
     create_join_table :categories, :service_providers do |t|
       # t.index [:category_id, :service_provider_id]
