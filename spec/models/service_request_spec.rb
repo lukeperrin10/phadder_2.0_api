e5923e7ea42b95ee53e0a7355062858d8ec950bc
kim_haaga <kim.haaga@live.se>
2021-10-29 11:10:10 +0200

    Add service_request associations to categories and bids

--- spec/models/service_request_spec.rb
+++ spec/models/service_request_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe ServiceRequest, type: :model do
 
     describe "associations" do
       it { is_expected.to belong_to :user }
-      # it { is_expected.to have_and_belong_to_many :categories }
-      # it { is_expected.to have_many :bids }
+      it { is_expected.to have_and_belong_to_many :categories }
+      it { is_expected.to have_many :bids }
       # it { is_expected.to have_one :deal }
     end
   end
