1372dd02129aba1c1ae1432e7f09c4731464cddb
kim_haaga <kim.haaga@live.se>
2021-11-01 10:04:23 +0100

    comment in tests for language code, add language code to session spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
-    #language_code { 'en' }
+    language_code { 'en' }
 
     after(:build) do |user|
       user.avatar.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'dummy_avatar.png')), filename: "avatar-#{user.email}.png", content_type: 'image/png')
--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'POST /api/auth/sign_in', type: :request do
         'allow_password_change' => false,
         'first_name' => user.first_name,
         'last_name' => user.last_name,
+        'language_code' => user.language_code,
         'role' => user.role,
         'service_provider_id' => nil
       }
