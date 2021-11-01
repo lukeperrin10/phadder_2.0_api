9e699a2c8256cfaf8d3a2a59177b8c23dbeac4f8
kim_haaga <kim.haaga@live.se>
2021-11-01 12:15:17 +0100

    Generate address, latitude, longitude column to service requests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_105120) do
+ActiveRecord::Schema.define(version: 2021_11_01_111237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,9 @@ ActiveRecord::Schema.define(version: 2021_11_01_105120) do
     t.integer "time_frame"
     t.text "languages", default: [], array: true
     t.string "location"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_service_requests_on_user_id"
   end
 
