76d44880fc3dadaa2925923bee7e631314b96686
sevader14 <vsevolod.deriushkin@gmail.com>
2021-11-02 11:03:45 +0100

    removes comments

--- app/serializers/categories/show_serializer.rb
+++ app/serializers/categories/show_serializer.rb
@@ -2,16 +2,4 @@
 
 class Categories::ShowSerializer < ActiveModel::Serializer
   attributes :id, :name
-  # attribute 
-
-  # def translations
-  #   translations = []
-  #   object.translations.each do |translation|
-  #     translations.push(
-  #       locale: translation.locale,
-  #       name: translation.name
-  #     )
-  #   end
-  #   translations
-  # end
 end
--- spec/requests/api/categories_spec.rb
+++ spec/requests/api/categories_spec.rb
@@ -2,18 +2,6 @@ RSpec.describe "Api::Categories", type: :request do
   describe "GET /api/categories" do
     let!(:consulting) {create(:category, name: 'Consulting')}
     let!(:accounting) {create(:category, name: 'Accounting')}
-    # let!(:consulting_translation) do
-    #   Category::Translation.create!(category_id: consulting.id,
-    #     locale: 'sv',
-    #     name: 'Konsulting')
-    # end
-
-
-    # let!(:accounting_translation) do
-    #   Category::Translation.create!(category_id: accounting.id,
-    #     locale: 'sv',
-    #     name: 'Bokföring')
-    # end
 
     before {get '/api/categories'}
     it "responds with 200" do
@@ -23,9 +11,5 @@ RSpec.describe "Api::Categories", type: :request do
     it 'responds with the right amount of objects' do 
       expect(response_json.count).to eq 2
     end
-
-    # it 'response include the swedish translation' do 
-    #   expect(response_json.first['translations'].count).to eq 2
-    # end
   end
 end
